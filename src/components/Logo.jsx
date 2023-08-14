31b758482399b26d7a3b34d416fbac4b490239b4
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-08-14 19:52:51 -0400

    small style fixes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,33 +2,37 @@
 // purpose: Logo component for social media icons
 import React from "react";
 import { Link } from 'react-router-dom';
+import gitLogo from '/public/img/logos/github.svg';
+import linkedinLogo from '/public/img/logos/linkedin.svg';
+import twitterLogo from '/public/img/logos/twitter.svg';
+import emailLogo from '/public/img/logos/email.svg';
 
 export default function Logo ({socialMedia}) {
     return (
       <Link to={socialMedia.url}>
         {socialMedia.name === 'GitHub' &&
-        <img src="src/assets/images/socialmedia/github.svg" 
+        <img src={gitLogo} 
                 alt="GitHub Logo" 
                 className="git-logo me-2"
                 style={{ width: '20px' }} needed                
                 />
         }
         {socialMedia.name === 'Linkedin' && 
-        <img src="src/assets/images/socialmedia/linkedin.svg"
+        <img src={linkedinLogo}
                 alt="LinkedIn Logo"
                 className="linkedin-logo me-2"
                 style={{ width: '20px' }} 
                 />
       }
       {socialMedia.name === 'Twitter' &&
-        <img src="src/assets/images/socialmedia/twitter.svg"
+        <img src={twitterLogo}
                 alt="Twitter Logo"
                 className="twitter-logo me-2"
                 style={{ width: '20px' }} 
                 />
       }
       {socialMedia.name === 'Email' &&
-        <img src="src/assets/images/socialmedia/email.svg"
+        <img src={emailLogo}
                 alt="Email Logo"
                 className="email-logo me-2"
                 style={{ width: '20px' }} 
